46f286023773f68075f25839178fa62c101ff4e9
tunahansalih <tusame1994>
2018-12-16 20:46:23 +0300

    add getmagnitude and getOrientation functions

--- hog.py
+++ hog.py
@@ -10,6 +10,11 @@ import cv2
 
 
 def getGradientX(image):
+    """
+
+    :param image: image matrix
+    :return: numpy array with the same shape of image
+    """
     gradient_x = np.zeros_like(image)
     gradient_x[:, 1:-1] = -image[:, :-2] + image[:, 2:]
     gradient_x[:, 0] = -image[:, 0] + image[:, 1]
@@ -19,6 +24,11 @@ def getGradientX(image):
 
 
 def getGradientY(image):
+    """
+
+    :param image: image matrix
+    :return: numpy array with the same shape of image
+    """
     gradient_y = np.zeros_like(image)
     gradient_y[1:-1, :] = image[:-2, :] - image[2:, :]
     gradient_y[0, :] = image[0, :] - image[1, :]
@@ -27,11 +37,42 @@ def getGradientY(image):
     return gradient_y
 
 
-def extractHoggFromImage(image, filter_width, filter_height, stride):
+def getMagnitude(gradientX, gradientY):
+    """
 
+    :param gradientX: numpy nd-array
+    :param gradientY: numpy nd-array
+    :return: numpy nd-array with the same shape of gradientX and gradientY
+    """
+    return np.sqrt(np.add(np.square(gradientX), np.square(gradientY)))
+
+
+def getOrientation(gradientX, gradientY):
+    """
+    :param gradientX: numpy nd-array
+    :param gradientY: numpy nd-array
+    :return: numpy nd-array with the same shape of gradientX and gradientY
+    """
+    return np.arctan2(gradientY, gradientX) * 180 / np.pi % 180
+
+
+def createHistogramFromMagnitudeAndOrientation(magnitude, orientation):
+
+    pass
+
+
+
+def extractHogFromImage(image, filter_width, filter_height, stride):
     gradient_x = getGradientX(image)
     gradient_y = getGradientY(image)
-    plt.imshow(gradient_x, cmap='gray')
+    magnitude = getMagnitude(gradient_x, gradient_y)
+    orientation = getOrientation(gradient_x, gradient_y)
+
+    plt.imshow(magnitude, cmap="gray")
     plt.show()
-    plt.imshow(gradient_y, cmap='gray')
+    plt.imshow(orientation, cmap="gray")
     plt.show()
+
+    for y in range(0, image.shape[0] - filter_width, step=stride):
+        for x in range(0, image.shape[1] - filter_height, step=stride):
+            pass
